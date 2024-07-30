68960147b8799ff70db179e5a6a08d7a2b829260
Nicolas Camacho <ncamacho@twilio.com>
2024-07-30 09:39:12 -0500

    changes for package release

--- src/TwilioPasskeys.js
+++ src/TwilioPasskeys.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2024 Twilio Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import CredentialManager from './CredentialManager';
 import CreatePasskeysResult from './models/CreatePasskeysResult';
 import AuthenticatePasskeysResult from './models/AuthenticatePasskeysResult';
@@ -9,7 +25,7 @@ import {
 } from './utils/mappers';
 
 /**
- * Class to manage Twilio Passkeys SDKs
+ * Class to manage Twilio Passkeys SDK
  */
 class TwilioPasskeys {
     /**
@@ -40,18 +56,18 @@ class TwilioPasskeys {
     }
 
     /**
-     * @param {string | AuthenticatePasskeysRequest} challengePayload
+     * @param {string | AuthenticatePasskeysRequest} authenticationRequest
      * @returns {Promise<AuthenticatePasskeysResult>}
     */
-    async authenticate(challengePayload) {
+    async authenticate(authenticationRequest) {
         let result = new AuthenticatePasskeysResult();
         
-        if (typeof(challengePayload) === 'string') {
-            challengePayload = mapToPasskeyAuthenticationPayload(challengePayload);
+        if (typeof(authenticationRequest) === 'string') {
+            authenticationRequest = mapToPasskeyAuthenticationPayload(authenticationRequest);
         }
 
         try {
-            let credential = await this.credentialManager.getCredential(challengePayload);
+            let credential = await this.credentialManager.getCredential(authenticationRequest);
             result.Success = mapToPasskeysAuthenticationResponse(credential);
         } catch (error) {
             result.Error = error;
