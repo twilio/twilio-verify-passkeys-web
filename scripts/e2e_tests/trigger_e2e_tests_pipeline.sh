4b034d9b6e46c3262ca8f56e975bf36b6e4dc046
Yeimi Moreno <ymoreno@twilio.com>
2025-05-12 12:48:21 -0500

    Improve param names. Execute e2e only for dev or by demand

--- scripts/e2e_tests/trigger_e2e_tests_pipeline.sh
+++ scripts/e2e_tests/trigger_e2e_tests_pipeline.sh
@@ -9,7 +9,7 @@ WEB_URL=$1
 TRIGGERING_WORKFLOW_ID=$2
 TRIGGERING_WORKFLOW_WAITING_JOB_NAME=$3
 
-echo "trigger-web-workflow"
-echo "triggering-workflow-id: $TRIGGERING_WORKFLOW_ID"
-echo "triggering-workflow-waiting-job-name: $TRIGGERING_WORKFLOW_WAITING_JOB_NAME"
+echo "trigger-web"
+echo "workflow-id: $TRIGGERING_WORKFLOW_ID"
+echo "waiting-job-name: $TRIGGERING_WORKFLOW_WAITING_JOB_NAME"
 echo "web_url: $WEB_URL"
