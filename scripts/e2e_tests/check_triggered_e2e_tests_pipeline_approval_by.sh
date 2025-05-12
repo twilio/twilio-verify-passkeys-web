9bcf14ba4cad4bec8e8a1ff5db9f169babf9d2b6
Yeimi Moreno <ymoreno@twilio.com>
2025-05-12 15:45:32 -0500

    Validation

--- scripts/e2e_tests/check_triggered_e2e_tests_pipeline_approval_by.sh
+++ scripts/e2e_tests/check_triggered_e2e_tests_pipeline_approval_by.sh
@@ -8,7 +8,7 @@ fi
 TRIGGERED_WORKFLOW=$1
 JOB_NAME_TO_CHECK=$2
 
-# Fetch the job status for the triggered workflow
+# Fetch the job's approval for the triggered workflow
 TRIGGERED_JOB_APPROVAL_BY=$(curl --request GET \
   --url "https://circleci.com/api/v2/workflow/${TRIGGERED_WORKFLOW}/job" \
   --header "Circle-Token: $CIRCLE_TOKEN" \
@@ -21,15 +21,11 @@ if [ -z "$TRIGGERED_JOB_APPROVAL_BY" ]; then
     exit 1
 fi
 
+# Get approver
 WHO_APPROVED=$(curl --request GET \
   --url "https://circleci.com/api/v2/user/${TRIGGERED_JOB_APPROVAL_BY}" \
   --header "Circle-Token: $CIRCLE_TOKEN" \
   --header "Content-Type: application/json" | \
   jq -r .login)
 
-if [[ "$WHO_APPROVED" != "$GITHUB_USER_NAME" ]]; then
-    echo "Workflow not successful - approved by ${WHO_APPROVED}"
-    (exit -1) 
-fi
-
-echo "APPROVED"
+echo $WHO_APPROVED
