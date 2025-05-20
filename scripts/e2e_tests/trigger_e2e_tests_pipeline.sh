2ddf96edf2c409f9e648e79cad1acfa1555a5f2d
Yeimi Fuquen <38441569+yafuquen@users.noreply.github.com>
2025-05-20 11:17:10 -0500

    chore: CI update (#16)

--- scripts/e2e_tests/trigger_e2e_tests_pipeline.sh
+++ scripts/e2e_tests/trigger_e2e_tests_pipeline.sh
@@ -9,30 +9,7 @@ WEB_URL=$1
 TRIGGERING_WORKFLOW_ID=$2
 TRIGGERING_WORKFLOW_WAITING_JOB_NAME=$3
 
-DATA_PAYLOAD=$(jq -n \
---arg triggering_workflow_id "$TRIGGERING_WORKFLOW_ID" \
---arg triggering_workflow_waiting_job_name "$TRIGGERING_WORKFLOW_WAITING_JOB_NAME" \
-'{
-    "branch": "main",
-    "parameters": {
-        "trigger-web-workflow": true,
-        "triggering-workflow-id": $triggering_workflow_id,
-        "triggering-workflow-waiting-job-name": $triggering_workflow_waiting_job_name
-    }
-}')
-
-DATA_PAYLOAD=$(echo "$DATA_PAYLOAD" | jq --arg web_url "$WEB_URL" '.parameters["web-url"]=$web_url')
-
-TRIGGERED_PIPELINE=$(curl --request POST \
-  --url https://circleci.com/api/v2/project/gh/twilio/twilio-verify-passkeys-tests/pipeline \
-  --header "Circle-Token: $CIRCLE_TOKEN" \
-  --header "Content-Type: application/json" \
-  --data "$DATA_PAYLOAD" | jq -r '.id')
-
-# Check if the pipeline creation was successful
-if [ -z "$TRIGGERED_PIPELINE" ]; then
-    echo "Failed to create pipeline"
-    exit 1
-fi
-
-echo "$TRIGGERED_PIPELINE"
+echo "trigger-web"
+echo "workflow-id: $TRIGGERING_WORKFLOW_ID"
+echo "waiting-job-name: $TRIGGERING_WORKFLOW_WAITING_JOB_NAME"
+echo "web_url: $WEB_URL"
